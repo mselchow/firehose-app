fb5734a12220549359cf8d17b69224e2920643da
Michael Selchow <michael@michael-VirtualBox.(none)>
2014-02-08 17:11:06 -0500

    Added the form.

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,5 +3,23 @@ class PicsController < ApplicationController
 	def index
 		@pics = Pic.all
 	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		# Pic.create(:hour => 1,
+		# 	:emotion => "",
+		#   :learned => "")
+		Pic.create(pics_params)
+		redirect_to pics_path
+	end
+
+	private
+
+	def pics_params
+		params.require(:pic).permit(:hour, :emotion, :learned)
+	end
 	
 end
